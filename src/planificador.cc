baf1abb32c363d46f0d7017bacf6fb86618c5ed4
Santiago Suarez Perez <suarez.santiago.perez@gmail.com>
2016-05-06 08:53:12 -0500

    Setting up processes and threads (errors still)

--- src/planificador.cc
+++ src/planificador.cc
@@ -1,13 +1,35 @@
-#include <unist.h>
+#include <unistd.h>
 #include <iostream>
+#include <map>
+#include <cstdlib>
 using namespace std;
-int main(int argc, char* argv){
-	extern char* optarg;
-	extern int optind, opterr, optopt;
-	int option;
-	while((option = getopt(argc, argv, "WDF?")) != -1){
-					switch(option){
-									case '':
-					}
-	}
+int main(int argc, char** argv){
+  extern char* optarg;
+  extern int optind, opterr, optopt;
+  int option;
+  int pcps = -1;
+  map<int, int> threadspcp;
+  while((option = getopt(argc, argv, "n:t:")) != -1){
+    switch(option){
+    case 'n':
+      cout << "Asignando numero de procesadores\n";
+      pcps = atoi(optarg);
+      for (int i=0; i<pcps; ++i)
+	threadspcp[i] = 3;
+      break;
+    case 't':
+      cout << "Asignando " << argv[optind] << " hilos al proceso " << optarg << endl;
+      int ts = atoi(argv[optind]);
+      int ns = atoi(optarg);
+      threadspcp[ns] = ts;
+      break;
+    default:
+      cout << "No se han asignado procesos ni hilos por defecto. Valor por defecto: 3\n";
+      pcps = 3;
+      threadspcp[0] = 3;
+      threadspcp[1] = 3;
+      threadspcp[2] = 3;
+      break;
+    }
+  }
 }
