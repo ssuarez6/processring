88dfea3f093f8904896212280f4afddcc1591f0c
Santiago Suarez Perez <suarez.santiago.perez@gmail.com>
2016-05-30 14:13:50 -0500

    fixed serialization and deserialization binaryly

--- src/proceso.cc
+++ src/proceso.cc
@@ -12,25 +12,25 @@ void Proceso::imprimirMensaje(){
 			write(STDOUT_FILENO, mensaje, sizeof(Mensaje));
 			//envia tareas
 			for(int i=0; i<mensaje->nTareas; ++i)
-							write(STDOUT_FILENO, mensaje->tareas[i], sizeof(Tarea));
+							write(STDOUT_FILENO, &(mensaje->tareas[i]), sizeof(Tarea));
 				//envia estadisticas
 			for(int i=0; i<mensaje->nEstadisticas; ++i)
-							write(STDOUT_FILENO, mensaje->estadisticas[i], 
+							write(STDOUT_FILENO, &(mensaje->estadisticas[i]), 
 															sizeof(Estadistica));
 }
 
 Mensaje* Proceso::leerMensaje(){
 				Mensaje* m;
 				while(STDIN_FILENO, m, sizeof(Mensaje)){
-								m->tareas = new Tarea*[m->nTareas];
-								m->estadisticas = new Estadistica*[m->nEstadisticas];
+								m->tareas[m->nTareas];
+								m->estadisticas[m->nEstadisticas];
 								for(int i=0; i<m->nTareas; ++i){
-												m->tareas[i] = new Tarea;
-												read(STDIN_FILENO, m->tareas[i], sizeof(Tarea));
+												m->tareas[i] = *(new Tarea);
+												read(STDIN_FILENO, &(m->tareas[i]), sizeof(Tarea));
 								}
 								for(int i=0; i<m->nEstadisticas; ++i){
-												m->estadisticas[i] = new Estadistica;
-												read(STDIN_FILENO, m->estadisticas[i],
+												m->estadisticas[i] = *(new Estadistica);
+												read(STDIN_FILENO, &(m->estadisticas[i]),
 																				sizeof(Estadistica));
 								}
 				}
