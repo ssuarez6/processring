ec557f763769d04ff152f56c65f1ee10e1f9e5de
Santiago Suarez Perez <suarez.santiago.perez@gmail.com>
2016-05-31 08:34:50 -0500

    making plp of test

--- src/plp.cc
+++ src/plp.cc
@@ -2,7 +2,7 @@
 //#include <time.h>
 #include <stdlib.h>
 #include <iostream>
-
+#include <string.h>
 using namespace std;
 
 int Plp::genRndmTareas(){
@@ -10,7 +10,26 @@ int Plp::genRndmTareas(){
 }
 
 void Plp::initMensaje(){
+	Mensaje* mensaje = this->getMensaje();
 
+	Tarea* t;
+	t->asignado = true;
+	char texto[64] = "una tarea a ejecutar";
+	memcpy(t->tareaAEjecutar, texto, strlen(texto)+1);
+	t->procesoId = 0;
+	t->hiloId = -1;
+
+	Estadistica* e;
+	memcpy(e->tareaAEjecutar, texto, strlen(texto)+1);
+	e->procesoId = 0;
+	e->hiloId = -1;
+
+	mensaje->nTareas = 1;
+	mensaje->nEstadisticas = 1;
+	mensaje->tareas[mensaje->nTareas];
+	mensaje->estadisticas[mensaje->nEstadisticas];
+	mensaje->tareas[0] = *t;
+	mensaje->estadisticas[0] = *e;
 }
 
 void Plp::generarTareas(){
@@ -20,7 +39,10 @@ void Plp::procesarMensaje(){}
 
 int main(){
 	Plp* me = new Plp();
-	cout << 0 << endl;
-	int n=-1;
-	cin >> n;
+	me->initMensaje();
+	me->printMessagetoErr();
+	me->imprimirMensaje();
+	me->leerMensaje();
+	me->printMessagetoErr();
+	return 0;
 }
